c06a8da8d8ea0215424492779b237106e21488e5
repinger <devel@repinger.my.id>
2023-05-04 23:42:47 +0700

    AnyKernel: Backup stuff before flashing
    Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -33,6 +33,19 @@ IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
+if mountpoint -q /data; then
+	ui_print " ";
+	ui_print "[+] Backing up boot & DTBO . . .";
+
+	dd if=/dev/block/by-name/boot of=/sdcard/backup_boot.img;
+	dd if=/dev/block/by-name/dtbo of=/sdcard/backup_dtbo.img;
+	if [ $? != 0 ]; then
+		ui_print "[!] Backup failed; proceeding anyway . . .";
+	else
+		ui_print "[+] Done";
+	fi
+fi
+
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
