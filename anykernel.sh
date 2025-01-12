14e669fdee8cba13f4e3080fc54d27f647192e05
Alexander Winkowski <dereference23@outlook.com>
2025-01-12 06:19:09 +0000

    AK3: Initialize custom methods

--- anykernel.sh
+++ anykernel.sh
@@ -52,6 +52,7 @@ fi
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
+. tools/ak3-custom.sh;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
---
+++ tools/ak3-custom.sh
@@ -0,0 +1,2 @@
+### AnyKernel custom methods
+## dereference23@github.com
